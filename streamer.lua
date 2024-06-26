079353771f107d348178161440236d6f1debb717
Armisael2k <chuylopez755@gmail.com>
2024-06-25 23:08:18 -0700

    Indentations fix

--- streamer.lua
+++ streamer.lua
@@ -3,7 +3,7 @@ StreamedVehicles = {};
 
 -- Evaluate a vehicle components
 function checkComponents(vehicle)
-	local components = getVehicleComponents(vehicle);
+    local components = getVehicleComponents(vehicle);
 
     if not StreamedVehicles[vehicle] then
         StreamedVehicles[vehicle] = {};
@@ -12,10 +12,10 @@ function checkComponents(vehicle)
     local stream = false;
     local lightStatus = areVehicleLightsOn(vehicle); -- Is not need to get every component
 
-	for component, visible in pairs(components) do
-		-- Popup Lights
-		local found = string.find(component, "f_pop");
-		if found then
+    for component, visible in pairs(components) do
+        -- Popup Lights
+        local found = string.find(component, "f_pop");
+        if found then
             stream = true;
 
             if not StreamedVehicles[vehicle].PopupLights then
@@ -25,17 +25,17 @@ function checkComponents(vehicle)
                 };
             end
 
-			local side = string.sub(component, found + 5, found + 5); -- 5 is length of f_pop.
+            local side = string.sub(component, found + 5, found + 5); -- 5 is length of f_pop.
             local props;
             local id = -1;
 
-			if side == "l" then -- Front left
+            if side == "l" then -- Front left
                 id = 0;
-				props = readPopupLight(component);
-			elseif side == "r" then -- Front right
+                props = readPopupLight(component);
+            elseif side == "r" then -- Front right
                 id = 1;
-				props = readPopupLight(component);
-			end
+                props = readPopupLight(component);
+            end
 
             if id ~= -1 then
                 resetVehicleComponentPosition(vehicle, component);
@@ -44,7 +44,7 @@ function checkComponents(vehicle)
                 local rx, ry, rz = getVehicleComponentRotation(vehicle, component, "parent");
                 local x, y, z = getVehicleComponentPosition(vehicle, component, "parent");
                 
-				if lightStatus then
+                if lightStatus then
                     setVehicleComponentRotation(vehicle, component, props.ax, props.ay, props.az, "parent");
                     setVehicleComponentPosition(vehicle, component, x + props.x, y + props.y, z + props.z, "parent");
                 end
@@ -66,9 +66,9 @@ function checkComponents(vehicle)
                 outputConsole("end =========="..id.."========")
                 outputConsole(inspect(StreamedVehicles))
             end
-		end
-		-- outputConsole(component);
-	end
+        end
+        -- outputConsole(component);
+    end
 
     if not stream then -- Prevent stream if component f_pop not valid and the vehicle is a new stream.
         StreamedVehicles[vehicle] = nil;
@@ -78,43 +78,43 @@ end
 
 -- Remove streamed vehicle.
 function destroyInstance(vehicle)
-	if StreamedVehicles[source] then
-		StreamedVehicles[source] = nil;
-		-- PENDING: Destroy more elements and logic if needed
-	end
+    if StreamedVehicles[source] then
+        StreamedVehicles[source] = nil;
+        -- PENDING: Destroy more elements and logic if needed
+    end
 end
 
 -- When a vehicle is streamed in by client.
 function handleStreamedIn()
-	if getElementType(source) == "vehicle" then
-		checkComponents(source);
-	end
+    if getElementType(source) == "vehicle" then
+        checkComponents(source);
+    end
 end
 addEventHandler("onClientElementStreamIn", root, handleStreamedIn);
 
 -- When a vehicle is streamed out by client.
 function handleStreamedOut()
-	destroyInstance(source);
+    destroyInstance(source);
 end
 addEventHandler("onClientElementStreamOut", root, handleStreamedOut);
 addEventHandler("onClientElementDestroy", root, handleStreamedOut);
 
 -- When a vehicle model change.
 function handleModelChange()
-	-- Remove current instance.
-	destroyInstance(source);
-	-- Re-evaluate if new model needs to be streamed.
-	checkComponents(source);
+    -- Remove current instance.
+    destroyInstance(source);
+    -- Re-evaluate if new model needs to be streamed.
+    checkComponents(source);
 end
 addEventHandler("onClientElementModelChange", root, handleModelChange);
 
 
 -- Detect streamed vehicles when resource start.
 function handleResourceStart()
-	local vehicles = getElementsByType("vehicle", root, true);
+    local vehicles = getElementsByType("vehicle", root, true);
 
-	for _, vehicle in ipairs(vehicles) do
-		checkComponents(vehicle);
-	end
+    for _, vehicle in ipairs(vehicles) do
+        checkComponents(vehicle);
+    end
 end
 addEventHandler("onClientResourceStart", resourceRoot, handleResourceStart);
